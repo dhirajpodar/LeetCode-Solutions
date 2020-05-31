848377e14c930ff6e3cae0782e664294e204fbce
dhirajpodar <50693861+dhirajpodar@users.noreply.github.com>
2020-05-31 14:26:03 +0545

    Create MaximumDepthOfBinaryTree.java

---
+++ MaximumDepthOfBinaryTree.java
@@ -0,0 +1,8 @@
+public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+        if(root.left == null && root.right == null){
+            return 1;
+        }else {
+            return Math.max(maxDepth(root.left),maxDepth(root.right)) + 1;
+        }
+    }
