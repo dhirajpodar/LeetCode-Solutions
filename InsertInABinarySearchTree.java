ade79e1af15446811be47e94285e88ee0b94999a
dhirajpodar <50693861+dhirajpodar@users.noreply.github.com>
2020-06-04 08:18:39 +0545

    Create InsertInABinarySearchTree.java

---
+++ InsertInABinarySearchTree.java
@@ -0,0 +1,10 @@
+public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root == null ){
+            return new TreeNode(val);
+        }else if(val < root.val){
+            root.left = insertIntoBST(root.left,val);
+        }else{
+            root.right = insertIntoBST(root.right,val);
+        }
+        return root;
+    }
